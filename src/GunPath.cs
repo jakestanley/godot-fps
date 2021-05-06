fa15eae95af9cf757a44005d4c5847170252f5bb
Jake Stanley <prudentia713@gmail.com>
2021-05-06 22:18:44 +0100

    view bobbing looks decent now

--- src/GunPath.cs
+++ src/GunPath.cs
@@ -7,14 +7,18 @@ public class GunPath : Godot.Path
     // private int a = 2;
     // private string b = "text";
     PathFollow follow;
+    private const float bobSpeed = 0.6f;
     private float bobDist = 0.2f;
+    private float length;
+    private bool neverMoved = true;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         follow = GetNode<PathFollow>("PathFollow");
         follow.RotationMode = PathFollow.RotationModeEnum.None;
-        
+
+        follow.Loop = false;
 
         Vector3 p0 = Vector3.Zero;
         Vector3 p0out = Vector3.Zero;
@@ -35,12 +39,30 @@ public class GunPath : Godot.Path
             Curve.AddPoint(p0, p1out, p0out);
             Curve.AddPoint(p1, p0out, p1out);
             Curve.AddPoint(p0, p1out, p0out);
+            length = Curve.GetBakedLength();
         }
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
- public override void _Process(float delta)
- {
-    follow.Offset += 0.5f * delta;
- }
+    public override void _Process(float delta)
+    {
+
+        if (Input.IsActionPressed("move_forward") || Input.IsActionPressed("move_backward") || Input.IsActionPressed("move_left") || Input.IsActionPressed("move_right")) {
+            follow.Loop = true;
+            follow.Offset += getBobSpeed(delta);
+            neverMoved = false;
+        } else if (!neverMoved){
+            follow.Loop = false;
+            if ((length/2 - follow.Offset) <= 0)
+            {
+                follow.Offset += getBobSpeed(delta);
+            } else {
+                follow.Offset -= getBobSpeed(delta);
+            }
+        } else {
+            follow.Offset = 0;
+        }
+    }
+    private float getBobSpeed(float delta) {
+        return bobSpeed * delta;
+    }
 }
